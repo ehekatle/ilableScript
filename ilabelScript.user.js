bcda1eade7e59772f1c621daaa7e162e9d297ee4
ehekatle <ehe@live.cn>
2025-12-17 01:40:27 +0800

    Update ilabelScript.user.js
    头文件更新

--- ilabelScript.user.js
+++ ilabelScript.user.js
@@ -4,7 +4,7 @@
 // @version      2.1
 // @description  预埋、豁免、直播信息违规、超时提示功能，最终版
 // @author       ehekatle
-// @source       https://github.com/ehekatle/ilableScript
+// @homepageURL  https://github.com/ehekatle/ilableScript
 // @updateURL    https://gh-proxy.org/https://github.com/ehekatle/ilableScript/raw/refs/heads/main/ilabelScript.meta.js
 // @downloadURL  https://gh-proxy.org/https://github.com/ehekatle/ilableScript/raw/refs/heads/main/ilabelScript.user.js
 // @match        https://ilabel.weixin.qq.com/*
