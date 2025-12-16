c146058fa36ad512de1fd280d3fe94aecc63800c
ehekatle <ehe@live.cn>
2025-12-17 01:36:39 +0800

    Update and rename ilabelScriptUser.js to ilabelScript.user.js
    头文件更新

--- ilabelScript.user.js
+++ ilabelScript.user.js
@@ -1,9 +1,12 @@
 // ==UserScript==
-// @name         iLabel直播审核辅助工具
-// @namespace    http://tampermonkey.net/
+// @name         iLabel直播审核辅助
+// @namespace    https://github.com/ehekatle/ilableScript
 // @version      2.1
 // @description  预埋、豁免、直播信息违规、超时提示功能，最终版
 // @author       ehekatle
+// @source       https://github.com/ehekatle/ilableScript
+// @updateURL    https://gh-proxy.org/https://github.com/ehekatle/ilableScript/raw/refs/heads/main/ilabelScript.meta.js
+// @downloadURL  https://gh-proxy.org/https://github.com/ehekatle/ilableScript/raw/refs/heads/main/ilabelScript.user.js
 // @match        https://ilabel.weixin.qq.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=weixin.qq.com
 // @grant        GM_xmlhttpRequest
