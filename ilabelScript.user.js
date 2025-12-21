77cf56de62231b0c286e3cffbeb84ff66bbe208d
ehekatle <ehe@live.cn>
2025-12-21 13:45:28 +0800

    Update ilabelScript.user.js
    地址修改

--- ilabelScript.user.js
+++ ilabelScript.user.js
@@ -8,7 +8,7 @@
 // @source       https://raw.githubusercontent.com/ehekatle/ilableScript/main/ilabelScript.user.js
 // @supportURL   https://github.com/ehekatle/ilableScript/issues
 // @updateURL    https://gh-proxy.org/https://raw.githubusercontent.com/ehekatle/ilableScript/main/ilabelScript.meta.js
-// @downloadURL  https://gh-proxy.org/https/raw.githubusercontent.com/ehekatle/ilableScript/main/ilabelScript.user.js
+// @downloadURL  https://gh-proxy.org/https://raw.githubusercontent.com/ehekatle/ilableScript/main/ilabelScript.user.js
 // @match        https://ilabel.weixin.qq.com/mixed-task/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=weixin.qq.com
 // @grant        GM_xmlhttpRequest
