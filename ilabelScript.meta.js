dffa8c7d92ce306c3d598bbe2a2343e84ebc798d
ehekatle <ehe@live.cn>
2025-12-31 17:36:01 +0800

    Update ilabelScript.meta.js
    闹钟加载更新

--- ilabelScript.meta.js
+++ ilabelScript.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         iLabel直播审核辅助
 // @namespace    https://github.com/ehekatle/ilableScript
-// @version      2.4.6
+// @version      2.4.7
 // @description  预埋、豁免、直播信息违规、超时提示功能，集成推送功能
 // @author       ehekatle
 // @homepage     https://github.com/ehekatle/ilableScript
